3f570affb33aa51392cd9367120ca9fbe64be8e6
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-08-20 21:45:26 +0200

    Add project files.

---
+++ MVC Basics 1 Empty/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Basics_1_Empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
